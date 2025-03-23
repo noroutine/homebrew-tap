3aafde90444efe68d56b4f478131ea6258bb0bf2
Oleksii Khilkevych <oleksiy@noroutine.me>
2025-03-23 18:25:13 +0100

    0.0.17

--- nail.rb
+++ nail.rb
@@ -2,9 +2,9 @@
 class Nail < Formula
   desc "Noroutine Application InterLink"
   homepage "https://nail.lane.id"
-  url "https://fs.noroutine.me/attic/dav/noroutine-nail-0.0.16.tgz"
-  sha256 "76f06792a986337d902b941c6177989e56f2d5250af68b8adf81c0622c1f471b"
-  version "0.0.16"
+  url "https://fs.noroutine.me/attic/dav/noroutine-nail-0.0.17.tgz"
+  sha256 "a87765767aba1d010903336a44d8832becbee1a45607ea0abfd811c3425e2e34"
+  version "0.0.17"
   
   depends_on "node"
   
